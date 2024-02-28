0f757d16256238f7ccb4b233179ad801f2a70e3c
Kbansheen <banshofficial123@gmail.com>
2024-02-28 13:36:09 +0530

    files

--- string_reverse_recursion.cpp
+++ string_reverse_recursion.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 using namespace std;
 
+void rev(string& str, int i, int j)
+{
+	//base case
+	if(i > j)
+	return;
+
+	swap(str[i], str[j]);
+	i++;
+	j--;
+	rev(str, i,j);
+}
+string reverseString(string& str)
+{
+	int i = 0;
+	int j = str.length()-1;
+	rev(str,i,j);
+	return str;
+
+}
+
 int main()
 {
-    cout<<"hello";
+    string str;
+    cout<<"enter the string";
+    cin>>str;
+    reverseString( str);
+    cout<<str;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+ cout<<"sorted array is:"<<endl;
+    for(int i = 0; i<5; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
